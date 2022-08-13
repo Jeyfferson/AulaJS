10400d84a216ab3f73a80ac3d0ae72be3b2db8ea
Jeyfferson <jeyffesilv@gmail.com>
2022-08-13 16:27:59 -0300

    training
    Signed-off-by: Jeyfferson <jeyffesilv@gmail.com>

--- Aula01/video17/js/app.js
+++
@@ -1,7 +0,0 @@
-console.log('Teste no console do JavaScript');
-let enptiness
-console.log(enptiness, enptiness + 3, `o valor é ${enptiness}`);
-
-let enptines = null //null = 0 / 0+3=3
-console.log(enptines, enptines + 3, `o valor é ${enptines}`);
-
